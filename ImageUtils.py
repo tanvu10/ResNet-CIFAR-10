94c6352f18bf0cdad28362556594bbaa29e28603
Vu Huynh <86453999+tanvu10@users.noreply.github.com>
2023-10-28 23:04:51 -0400

    Add files via upload

---
+++ ImageUtils.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+""" This script implements the functions for data augmentation and preprocessing.
+"""
+
+def parse_record(record, training):
+    """ Parse a record to an image and perform data preprocessing.
+
+    Args:
+        record: An array of shape [3072,]. One row of the x_* matrix.
+        training: A boolean. Determine whether it is in training mode.
+
+    Returns:
+        image: An array of shape [3, 32, 32].
+    """
+    # Reshape from [depth * height * width] to [depth, height, width].
+    depth_major = record.reshape((3, 32, 32))
+
+    # Convert from [depth, height, width] to [height, width, depth]
+    image = np.transpose(depth_major, [1, 2, 0])
+
+    image = preprocess_image(image, training)
+
+    # Convert from [height, width, depth] to [depth, height, width]
+    image = np.transpose(image, [2, 0, 1])
+
+    return image
+
+def preprocess_image(image, training):
+    """ Preprocess a single image of shape [height, width, depth].
+
+    Args:
+        image: An array of shape [32, 32, 3].
+        training: A boolean. Determine whether it is in training mode.
+    
+    Returns:
+        image: An array of shape [32, 32, 3].
+    """
+    if training:
+        pass
+        ### YOUR CODE HERE
+        # Resize the image to add four extra pixels on each side.
+        
+        ### YOUR CODE HERE
+
+        ### YOUR CODE HERE
+        # Randomly crop a [32, 32] section of the image.
+        # HINT: randomly generate the upper left point of the image
+        
+        ### YOUR CODE HERE
+
+        ### YOUR CODE HERE
+        # Randomly flip the image horizontally.
+        
+        ### YOUR CODE HERE
+        
+    ### YOUR CODE HERE
+    # Subtract off the mean and divide by the standard deviation of the pixels.
+    
+    ### YOUR CODE HERE
+
+    return image
---
+++ Model.py
@@ -0,0 +1,95 @@
+import os
+import time
+import torch
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+
+from NetWork import ResNet
+from ImageUtils import parse_record
+
+""" This script defines the training, validation and testing process.
+"""
+
+class Cifar(nn.Module):
+    def __init__(self, config):
+        super(Cifar, self).__init__()
+        self.config = config
+        self.network = ResNet(
+            self.config.resnet_version,
+            self.config.resnet_size,
+            self.config.num_classes,
+            self.config.first_num_filters,
+        )
+        ### YOUR CODE HERE
+        # define cross entropy loss and optimizer
+
+        ### YOUR CODE HERE
+    
+    def train(self, x_train, y_train, max_epoch):
+        self.network.train()
+        # Determine how many batches in an epoch
+        num_samples = x_train.shape[0]
+        num_batches = num_samples // self.config.batch_size
+
+        print('### Training... ###')
+        for epoch in range(1, max_epoch+1):
+            start_time = time.time()
+            # Shuffle
+            shuffle_index = np.random.permutation(num_samples)
+            curr_x_train = x_train[shuffle_index]
+            curr_y_train = y_train[shuffle_index]
+
+            ### YOUR CODE HERE
+            # Set the learning rate for this epoch
+            # Usage example: divide the initial learning rate by 10 after several epochs
+            
+            ### YOUR CODE HERE
+            
+            for i in range(num_batches):
+                ### YOUR CODE HERE
+                # Construct the current batch.
+                # Don't forget to use "parse_record" to perform data preprocessing.
+                # Don't forget L2 weight decay
+                
+                ### YOUR CODE HERE
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                print('Batch {:d}/{:d} Loss {:.6f}'.format(i, num_batches, loss), end='\r', flush=True)
+            
+            duration = time.time() - start_time
+            print('Epoch {:d} Loss {:.6f} Duration {:.3f} seconds.'.format(epoch, loss, duration))
+
+            if epoch % self.config.save_interval == 0:
+                self.save(epoch)
+
+
+    def test_or_validate(self, x, y, checkpoint_num_list):
+        self.network.eval()
+        print('### Test or Validation ###')
+        for checkpoint_num in checkpoint_num_list:
+            checkpointfile = os.path.join(self.config.modeldir, 'model-%d.ckpt'%(checkpoint_num))
+            self.load(checkpointfile)
+
+            preds = []
+            for i in tqdm(range(x.shape[0])):
+                ### YOUR CODE HERE
+                
+                ### END CODE HERE
+
+            y = torch.tensor(y)
+            preds = torch.tensor(preds)
+            print('Test accuracy: {:.4f}'.format(torch.sum(preds==y)/y.shape[0]))
+    
+    def save(self, epoch):
+        checkpoint_path = os.path.join(self.config.modeldir, 'model-%d.ckpt'%(epoch))
+        os.makedirs(self.config.modeldir, exist_ok=True)
+        torch.save(self.network.state_dict(), checkpoint_path)
+        print("Checkpoint has been created.")
+    
+    def load(self, checkpoint_name):
+        ckpt = torch.load(checkpoint_name, map_location="cpu")
+        self.network.load_state_dict(ckpt, strict=True)
+        print("Restored model parameters from {}".format(checkpoint_name))
