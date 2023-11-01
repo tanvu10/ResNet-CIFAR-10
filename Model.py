203cf696bf0bb809589772bc8356d49b0dce2f1a
Vu Tan Huynh <vuh@Vu-Hs-MacBook-Air.local>
2023-10-31 23:50:34 -0400

    finish model

--- Model.py
+++ Model.py
@@ -23,6 +23,17 @@ class Cifar(nn.Module):
         )
         ### YOUR CODE HERE
         # define cross entropy loss and optimizer
+        self.criterion = nn.CrossEntropyLoss()
+
+        # add L2 weight decay in the optimizer
+        self.optimizer = torch.optim.SGD(self.network.parameters(), 
+                                         lr=self.config.learning_rate, 
+                                         weight_decay=self.config.weight_decay)
+        
+        # add feature lr_decay by  = 1/10 every lr_decay_step
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 
+                                                         step_size=self.config.lr_decay_step, 
+                                                         gamma=0.1)
 
         ### YOUR CODE HERE
     
@@ -43,7 +54,7 @@ class Cifar(nn.Module):
             ### YOUR CODE HERE
             # Set the learning rate for this epoch
             # Usage example: divide the initial learning rate by 10 after several epochs
-            
+            self.scheduler.step()
             ### YOUR CODE HERE
             
             for i in range(num_batches):
@@ -51,10 +62,25 @@ class Cifar(nn.Module):
                 # Construct the current batch.
                 # Don't forget to use "parse_record" to perform data preprocessing.
                 # Don't forget L2 weight decay
+                start = i * self.config.batch_size
+                end = start + self.config.batch_size
+
+                x_batch = np.array([parse_record(image, training=True) for image in curr_x_train[start:end]])
+                y_batch = curr_y_train[start:end]
+
+                # change to tensor type
+                x_batch = torch.tensor(x_batch, dtype=torch.float32)
+                y_batch = torch.tensor(y_batch, dtype=torch.int32)
+
+                outputs = self.network(x_batch)
+                loss = self.criterion(outputs, y_batch)
                 
                 ### YOUR CODE HERE
+                # reset gradients for each batch
                 self.optimizer.zero_grad()
+                # back propagation
                 loss.backward()
+                # update params
                 self.optimizer.step()
 
                 print('Batch {:d}/{:d} Loss {:.6f}'.format(i, num_batches, loss), end='\r', flush=True)
@@ -62,21 +88,34 @@ class Cifar(nn.Module):
             duration = time.time() - start_time
             print('Epoch {:d} Loss {:.6f} Duration {:.3f} seconds.'.format(epoch, loss, duration))
 
+            # save all params every save_interval_epoch
             if epoch % self.config.save_interval == 0:
                 self.save(epoch)
 
 
     def test_or_validate(self, x, y, checkpoint_num_list):
+        # change to test mode (since using BN params from training process)
         self.network.eval()
         print('### Test or Validation ###')
         for checkpoint_num in checkpoint_num_list:
-            checkpointfile = os.path.join(self.config.modeldir, 'model-%d.ckpt'%(checkpoint_num))
-            self.load(checkpointfile)
+            checkpoint_file = os.path.join(self.config.modeldir, 'model-%d.ckpt'%(checkpoint_num))
+            self.load(checkpoint_file)
 
             preds = []
             for i in tqdm(range(x.shape[0])):
                 ### YOUR CODE HERE
-                
+                # avoid calculating gradient
+                with torch.no_grad():
+                    image = parse_record(x[i], training=False)
+                    # change dimension from [H, W, C] to [1, H, W, C]
+                    image = image.squeeze(0)
+                    image = torch.tensor(image, dtype=torch.float32)
+
+                    output = self.network(image)
+                    prob = nn.functional.softmax(output, dim=1)
+                    pred_class = torch.argmax(prob, dim=1)
+                    # take out scalar value
+                    preds.append(pred_class.item())
                 ### END CODE HERE
 
             y = torch.tensor(y)
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import argparse
 
 def configure():
     parser = argparse.ArgumentParser()
-    ### YOUR CODE HERE
+    ### YOUR CODE HERE 18
     parser.add_argument("--resnet_version", type=int, default=1, help="the version of ResNet")
-    parser.add_argument("--resnet_size", type=int, default=18, 
+    parser.add_argument("--resnet_size", type=int, default=2, 
                         help='n: the size of ResNet-(6n+2) v1 or ResNet-(9n+2) v2')
     parser.add_argument("--batch_size", type=int, default=128, help='training batch size')
     parser.add_argument("--num_classes", type=int, default=10, help='number of classes')
@@ -18,6 +18,7 @@ def configure():
     parser.add_argument("--first_num_filters", type=int, default=16, help='number of classes')
     parser.add_argument("--weight_decay", type=float, default=2e-4, help='weight decay rate')
     parser.add_argument("--modeldir", type=str, default='model_v1', help='model directory')
+    parser.add_argument("--lr_decay_step", type=int, default=10, help='number of step (epoch) to decay lr')
     ### YOUR CODE HERE
     return parser.parse_args()
 
@@ -25,9 +26,10 @@ def main(config):
     print("--- Preparing Data ---")
 
     ### YOUR CODE HERE
-    data_dir = ""
-    ### YOUR CODE HERE
+    # data_dir = "/Users/vuh/Downloads/cifar-10-batches-py/"
+    data_dir = '/gpfs/scratch/vhuynh/cifar-10-batches-py/'
 
+    ### YOUR CODE HERE
     x_train, y_train, x_test, y_test = load_data(data_dir)
     x_train_new, y_train_new, x_valid, y_valid = train_vaild_split(x_train, y_train)
 
@@ -35,8 +37,8 @@ def main(config):
 
     ### YOUR CODE HERE
     # First step: use the train_new set and the valid set to choose hyperparameters.
-    # model.train(x_train_new, y_train_new, 200)
-    # model.test_or_validat`e(x_valid, y_valid, [160, 170, 180, 190, 200])
+    model.train(x_train_new, y_train_new, 200)
+    model.test_or_validate(x_valid, y_valid, [160, 170, 180, 190, 200])
 
     # Second step: with hyperparameters determined in the first run, re-train
     # your model on the original train set.
