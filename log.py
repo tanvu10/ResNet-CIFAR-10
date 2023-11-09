fc58169fc6fc3477724619a5dbe0453a0d60015e
Vu Tan Huynh <vuh@Vu-Hs-MacBook-Air.local>
2023-11-08 23:36:29 -0500

    run on a100

---
+++ log.py
@@ -0,0 +1,14 @@
+class DualLogger:
+    def __init__(self, filepath):
+        self.terminal = sys.stdout
+        self.log = open(filepath, "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        # This flush method is needed for python 3 compatibility.
+        # This handles the flush command by doing nothing.
+        # You might want to specify some extra behavior here.
+        pass
